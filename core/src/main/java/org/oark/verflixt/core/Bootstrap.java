f24f90cec852208901d94bcf0f4628c04d32906b
Simon Kölsch <echox@echox.de>
2013-08-20 01:48:50 +0200

    added basic spring application context

--- core/src/main/java/org/oark/verflixt/core/Bootstrap.java
+++ core/src/main/java/org/oark/verflixt/core/Bootstrap.java
@@ -2,10 +2,19 @@ package org.oark.verflixt.core;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Bootstrap {
 
 	private static Logger log = LoggerFactory.getLogger(Bootstrap.class);
+	
+	private static final ApplicationContext context = new ClassPathXmlApplicationContext("application.xml");
+
+    public static ApplicationContext getContext()
+    {
+        return context;
+    }
 	
 	public static void main(String[] args) {
 		log.info("bootstrapping application...");
