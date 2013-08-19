1ef3e327045b32d0434379eb21d159c556bfff10
Simon Kölsch <echox@echox.de>
2013-08-20 01:39:11 +0200

    boostrapp class

---
+++ core/src/main/java/org/oark/verflixt/core/Bootstrap.java
@@ -0,0 +1,14 @@
+package org.oark.verflixt.core;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Bootstrap {
+
+	private static Logger log = LoggerFactory.getLogger(Bootstrap.class);
+	
+	public static void main(String[] args) {
+		log.info("bootstrapping application...");
+	}
+
+}
