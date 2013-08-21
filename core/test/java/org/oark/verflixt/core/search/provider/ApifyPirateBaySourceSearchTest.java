40d854c33dfb0a1fa9e3c4c7945cfbe02e8df8e3
Simon Kölsch <echox@echox.de>
2013-08-21 23:45:38 +0200

    junit setup

---
+++ core/test/java/org/oark/verflixt/core/search/provider/ApifyPirateBaySourceSearchTest.java
@@ -0,0 +1,11 @@
+package org.oark.verflixt.core.search.provider;
+
+import org.junit.Test;
+
+public class ApifyPirateBaySourceSearchTest {
+
+	@Test
+	public void searchTest() {
+		//TODO just a test example
+	}
+}
