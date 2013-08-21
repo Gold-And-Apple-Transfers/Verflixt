429df07ccee0dfd36aab38f9719f73885a8a328f
Simon Kölsch <echox@echox.de>
2013-08-21 22:44:53 +0200

    Apify: get api key from system property

--- core/src/main/java/org/oark/verflixt/core/search/provider/ApifyPirateBaySourceSearch.java
+++ core/src/main/java/org/oark/verflixt/core/search/provider/ApifyPirateBaySourceSearch.java
@@ -1,14 +1,38 @@
 package org.oark.verflixt.core.search.provider;
 
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ApifyPirateBaySourceSearch extends SourceSearchProvider {
 
+	Logger log = LoggerFactory.getLogger(ApifyPirateBaySourceSearch.class);
+	
+	@Value("#{ systemProperties['apify.key'] }")
+	private String apikey = "SET PROPERTY apify.key";
+	
+	
+	@PostConstruct
+	public void init() {
+		log.info(ApifyPirateBaySourceSearch.class + " initialized with API KEY: " + apikey);
+	}
+	
 	@Override
 	void search(String query) {
 		
 	}
 
+	public String getApikey() {
+		return apikey;
+	}
 
+	public void setApikey(String apikey) {
+		this.apikey = apikey;
+	}
+
+	
 }
