b40f77dd01e68d871c297f7e66bb9089f80f8a0a
Simon Elsbrock <simon@iodev.org>
2013-08-21 21:39:24 +0200

    define StorageProvider interface
    first draft of SimpleStorage provider

--- core/src/main/java/org/oark/verflixt/core/storage/provider/SimpleStorage.java
+++ core/src/main/java/org/oark/verflixt/core/storage/provider/SimpleStorage.java
@@ -1,5 +1,42 @@
 package org.oark.verflixt.core.storage.provider;
 
-public class SimpleStorage implements StorageProvider {
+import java.io.File;
+import java.io.InputStream;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SimpleStorage implements StorageProvider {
+	@Autowired
+	private File storagePath;
+
+	public boolean write(InputStream input, String target) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean delete(String target) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean read(String source) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public DirectoryListing ls(String root) {
+		File absoluteRoot = new File(storagePath, root);
+		
+		return null;
+	}
+
+	public File getStoragePath() {
+		return storagePath;
+	}
+
+	public void setStoragePath(File storagePath) {
+		this.storagePath = storagePath;
+	}
 }
--- core/src/main/java/org/oark/verflixt/core/storage/provider/StorageProvider.java
+++ core/src/main/java/org/oark/verflixt/core/storage/provider/StorageProvider.java
@@ -1,5 +1,10 @@
 package org.oark.verflixt.core.storage.provider;
 
-public interface StorageProvider {
+import java.io.InputStream;
 
+public interface StorageProvider {
+	public boolean write(InputStream input, String target);
+	public boolean delete(String target);
+	public boolean read(String source);
+	public DirectoryListing ls(String root);
 }
