bbc3e1fd92a040433f5d21ebf1bfb3e4a5ba92a7
Simon Kölsch <echox@echox.de>
2013-08-25 21:41:53 +0200

    removed auto generated main

--- core/src/main/java/org/oark/verflixt/core/search/provider/ContentSearchProvider.java
+++ core/src/main/java/org/oark/verflixt/core/search/provider/ContentSearchProvider.java
@@ -2,12 +2,4 @@ package org.oark.verflixt.core.search.provider;
 
 public class ContentSearchProvider extends SearchProvider {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
 }
