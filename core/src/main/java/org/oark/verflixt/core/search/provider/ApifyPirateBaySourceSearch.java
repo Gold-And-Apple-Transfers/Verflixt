5bfa9c5a8392bcc490a39f83578c957323bea8d4
Simon Kölsch <echox@echox.de>
2013-08-27 21:38:54 +0200

    refactored JSON calls into an own class

--- core/src/main/java/org/oark/verflixt/core/search/provider/ApifyPirateBaySourceSearch.java
+++ core/src/main/java/org/oark/verflixt/core/search/provider/ApifyPirateBaySourceSearch.java
@@ -1,14 +1,8 @@
 package org.oark.verflixt.core.search.provider;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
 import java.io.UnsupportedEncodingException;
-import java.net.URL;
 import java.net.URLEncoder;
-import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Date;
@@ -19,8 +13,10 @@ import javax.annotation.PostConstruct;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.oark.verflixt.core.JSONReader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -49,7 +45,7 @@ public class ApifyPirateBaySourceSearch extends SourceSearchProvider {
 		
 		try {
 			JSONArray jsonResult;
-			jsonResult = readJsonFromUrl(buildURI(query));
+			jsonResult = JSONReader.readJsonFromUrl(buildURI(query));
 
 			for (int i = 0; i < jsonResult.length(); i++) {
 				JSONObject item = jsonResult.getJSONObject(i);
@@ -91,29 +87,6 @@ public class ApifyPirateBaySourceSearch extends SourceSearchProvider {
 		this.apikey = apikey;
 	}
 
-	private static String readAll(Reader rd) throws IOException {
-		StringBuilder sb = new StringBuilder();
-		int cp;
-		while ((cp = rd.read()) != -1) {
-			sb.append((char) cp);
-		}
-		return sb.toString();
-	}
-
-	private static JSONArray readJsonFromUrl(String url) throws IOException,
-			JSONException {
-		InputStream is = new URL(url).openStream();
-		try {
-			BufferedReader rd = new BufferedReader(new InputStreamReader(is,
-					Charset.forName("UTF-8")));
-			String jsonText = readAll(rd);
-			JSONArray json = new JSONArray(jsonText);
-			return json;
-		} finally {
-			is.close();
-		}
-	}
-
 	public String getTpbServiceBase() {
 		return tpbServiceBase;
 	}
@@ -121,5 +94,4 @@ public class ApifyPirateBaySourceSearch extends SourceSearchProvider {
 	public void setTpbServiceBase(String tpbServiceBase) {
 		this.tpbServiceBase = tpbServiceBase;
 	}
-
 }
